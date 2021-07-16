9d0596f073b0434c43a63f48628ce8af8e1057b7
Nayemul <nayemulhasan2016@gmail.com>
2021-07-17 01:45:39 +0600

    shoe message client clg emit()

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //run when client connects
 io.on('connection', socket =>{
     console.log('new socket connected!');
+    socket.emit('message',"welcome sChat");
 })
 
 
